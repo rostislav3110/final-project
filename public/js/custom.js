1359993b009e22e7fb0476302766ce797bdc2da5
rostislav <110746897+rostislav3110@users.noreply.github.com>
2023-07-10 00:14:13 +0300

    v1.0.0

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,4 @@
 
-let tablinks = document.getElementsByClassName("tab-links");
-let tabcontents = document.getElementsByClassName("tab-contents");
 let sidemenu = document.getElementById("sidemenu");
 const goTopBtn = document.querySelector(".button-home");
 
@@ -15,18 +13,6 @@ function trackScroll() {
     goTopBtn.classList.remove("button-home--show");
   }
 };
-
-function opentab(tabname) {
-  for (link of tablinks) {
-    link.classList.remove("active-links");
-  }
-  for (content of tabcontents) {
-    content.classList.remove("active-tab");
-  }
-  event.currentTarget.classList.add("active-links");
-  document.getElementById(tabname).classList.add("active-tab");
-};
-
 
 function openmenu() {
   sidemenu.style.right = "0";
